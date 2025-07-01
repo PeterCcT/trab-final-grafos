a0dd37a54f3c630e4db85de68c28977fa548dbb3
PeterCcT <pedro.rodrigues@syngenta.com>
2025-06-30 23:12:43 -0300

    feat: added missing req

--- scripts/fill_dataset/run.py
+++ scripts/fill_dataset/run.py
@@ -1,8 +1,6 @@
 from scripts.fill_dataset.get_prs import fetch_all_prs
-
+from scripts.fill_dataset.get_issues import fetch_all_issues
 if __name__ == '__main__':
-    # from scripts.fill_dataset.get_issues import fetch_all_issues
-    # fetch_all_issues()
+    fetch_all_issues()
     fetch_all_prs()
-
     print("Dataset filled successfully.")
